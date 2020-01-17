817a9ffc8513e682024d857dc327ba0f7d961f9e
Fraddy Oliveira <fraddy071@gmail.com>
2020-01-17 06:41:03 +0530

    add layout component and integrate it with app component. add Aux hoc.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Layout from './components/Layout/Layout'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div>
+            <Layout>
+                <p>Test</p>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Aux from './../../hoc/Aux'
+
+const layout = props => {
+    return (
+        <Aux>
+            <div>
+                Toolbar, Order
+            </div>
+            <main>
+                {props.children}
+            </main>
+        </Aux>
+    )
+}
+
+export default layout;
