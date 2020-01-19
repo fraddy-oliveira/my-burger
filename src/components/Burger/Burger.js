080f1ac31937e90327beccdc02dc04bfa8292d7d
Fraddy Oliveira <fraddy071@gmail.com>
2020-01-19 12:45:04 +0530

    Generic code to display burger ingredient
    Fetch count of ingredients to display from BurgerBuilder container.
    
    Make ingredients generic.
    
    Maintain ingredients as state property.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import PropTypes from 'prop-types'
 
 const burger = props => {
+
+    const transformedIngredient = Object.keys(props.ingredients).map(key => {
+        return [...Array(props.ingredients[key])].map((_, index) => {
+            return <BurgerIngredient key={key + index} type={key} />
+        })
+    })
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="salad" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="bacon" />
+            {transformedIngredient}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
+
+}
+
+burger.propTypes = {
+    ingredients: PropTypes.objectOf(PropTypes.number)
 }
 
 export default burger;
