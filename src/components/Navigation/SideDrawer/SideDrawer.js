cbd9466c762bbcb4f280a50505bf42fd0edcbf33
Fraddy Oliveira <fraddy071@gmail.com>
2020-01-26 10:02:31 +0530

    Add logo, sidedrawer, navigation items

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import classes from './Toolbar.css'
 import Logo from './../../Logo/Logo'
 import NavigationItems from './../NavigationItems/NavigationItems'
 
-const toolbar = (props) => {
+import classes from './SideDrawer.css'
+
+const sideDrawer = (props) => {
     return (
-        <header className={classes.Toolbar}>
-            <div>MENU</div>
+        <div className={classes.SideDrawer}>
             <div className={classes.Logo}>
                 <Logo />
             </div>
-            <nav className={classes.DesktopOnly}>
+            <nav>
                 <NavigationItems />
             </nav>
-        </header>
+        </div>
     )
 }
 
-export default toolbar
+export default sideDrawer;
