22e327db884b9e169f96895aaaab92f1d5d3d85d
Fraddy Oliveira <fraddy071@gmail.com>
2020-01-26 13:02:55 +0530

    Toggle sidedrawer, show hambuger icon
    Add ability to show and hide mobile menu (sidedrawer).
    Hide Menu word which is present in navigation on desktop devices.
    Replace Menu word with hamburger icon.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from './../../hoc/Aux'
 import classes from './Layout.css'
 import Toolbar from './../Navigation/Toolbar/Toolbar'
 import SideDrawer from './../Navigation/SideDrawer/SideDrawer'
 
-const layout = props => {
-    return (
-        <Aux>
-            <Toolbar />
-            <SideDrawer />
-            <main className={classes.container} >
-                {props.children}
-            </main>
-        </Aux>
-    )
+class Layout extends Component {
+
+    state = {
+        openSideDrawer: false
+    }
+
+    closeSideDrawerHandler = () => {
+        this.setState({ openSideDrawer: false })
+    }
+
+    toggleSideDrawerHandler = () => {
+        this.setState((prevState) => {
+            return { openSideDrawer: !prevState.openSideDrawer }
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar toggleSideDrawer={this.toggleSideDrawerHandler} />
+                <SideDrawer show={this.state.openSideDrawer} close={this.closeSideDrawerHandler} />
+                <main className={classes.container} >
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
 }
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import Logo from './../../Logo/Logo'
 import NavigationItems from './../NavigationItems/NavigationItems'
+import Aux from './../../../hoc/Aux'
+import Backdrop from './../../UI/Backdrop/Backdrop'
 
 import classes from './SideDrawer.css'
 
 const sideDrawer = (props) => {
+
+    let sideDrawerClasses = [classes.SideDrawer]
+
+    if (props.show) {
+        sideDrawerClasses.push(classes.Open)
+    } else {
+        sideDrawerClasses.push(classes.Close)
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.show} close={props.close} />
+            <div className={sideDrawerClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     )
 }
 
