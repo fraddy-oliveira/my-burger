a5e0c6580502991cd74e854d57de9b42eea9ad39
Fraddy Oliveira <fraddy071@gmail.com>
2020-04-26 16:03:17 +0530

    Link React Devtools extension to application.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import * as serviceWorker from './serviceWorker';
 import App from './App';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
   <Provider store={store}>
